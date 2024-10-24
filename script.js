06fd8a16cfa5b7b96214a47da6aa099a72541b09
Teodor-15 <marthagilmore65@outlook.com>
2024-10-24 00:05:34 +0000

    Improve code quality.

---
+++ script.js
@@ -0,0 +1,30 @@
+const board = document.getElementById('game-board');
+const statusDisplay = document.getElementById('status');
+const restartButton = document.getElementById('restart');
+
+let gameActive = true;
+ // let gameActive = true;let currentPlayer = 'X';
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellClick(clickedCell, clickedCellIndex) {
+    if (gameState[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    }
+
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+
+    checkResult();
+}
+
